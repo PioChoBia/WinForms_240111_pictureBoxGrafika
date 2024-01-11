602f5c30b4fe16eccb92b3570969cd0769793468
Pio <pio.cho.bia@gmail.com>
2024-01-11 19:57:36 +0100

    częśc1

--- Form1.cs
+++ Form1.cs
@@ -1,15 +1,19 @@
+using System.Security.Cryptography.X509Certificates;
+
 namespace WinForms_240111_pictureBoxGrafika
 {
   public partial class Form1 : Form
-  {
+  {  
+
     public Form1()
     {
       InitializeComponent();
       pictureBox1.Size=new Size(400,400);
       pictureBox1.Location = new Point(10, 10);
-      pictureBox1.BackColor= Color.Gray;
+      //pictureBox1.BackColor= Color.Gray;
     }
 
+  
     //rysuje na pocz¹tku i po ka¿dym Refresh()
     private void pictureBox1_Paint(object sender, PaintEventArgs e)
     {
@@ -26,12 +30,17 @@ namespace WinForms_240111_pictureBoxGrafika
 
     private void buttonOdcinek_Click(object sender, EventArgs e)
     {
-
+      pictureBox1.CreateGraphics().DrawLine(
+        new Pen(Color.Black,3),
+        new Point(0, 0), new Point(399, 399));
     }
 
     private void buttonProstokat_Click(object sender, EventArgs e)
     {
-
+      Graphics g = pictureBox1.CreateGraphics();
+      g.DrawRectangle(
+        Pens.Brown,
+        new Rectangle(new Point(10, 10), new Size(50, 80)) );
     }
 
     private void buttonElipsa_Click(object sender, EventArgs e)
